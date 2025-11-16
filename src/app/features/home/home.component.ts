4554c2729b7041307ccbbafd0d48c066b707894b
farah mzoughi <farahmzoughi11@gmail.com>
2025-11-16 15:23:39 +0100

    Start working on home section

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
